f7008dbb45c21da28fea3da45227145d391aa8da
DuarteLeitao1 <>
2022-06-28 10:29:33 +0000

    new reddit command

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -17,5 +17,15 @@ class Reddit(commands.Cog):
                 embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
                 await ctx.send(embed=embed)
 
+    @commands.command(pass_context=True, brief = "-> F1 wallpaper from r/F1Porn")
+    async def f1(self, ctx):
+        embed = discord.Embed(title="", description="")
+
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/F1Porn/new.json?sort=hot') as r:
+                res = await r.json()
+                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+                await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Reddit(bot))
